51daaba0b443282563bc03e096d43e855e494628
princemuysser-jpg <prince.muysser@gmail.com>
2026-07-08 23:19:23 +0300

    Delete supabase_v28.sql

--- supabase_v28.sql
+++
@@ -1,8 +0,0 @@
--- Alin V28 Brand Manager
--- لا يحتاج إنشاء جداول جديدة إذا كان جدول settings موجود من الإصدارات السابقة.
--- هذه الأوامر تضيف مفاتيح الهوية الافتراضية فقط بدون حذف أي بيانات.
-insert into settings (key, value) values
-('platform_logo_path',''),
-('platform_icon_path',''),
-('platform_short_name','آلين')
-on conflict (key) do nothing;
