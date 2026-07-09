44d4c7639101a3b92834eaca7a639e40b32cbba6
princemuysser-jpg <prince.muysser@gmail.com>
2026-07-09 16:17:29 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-// ALIN V53: service worker disabled to prevent old cache
-self.addEventListener('install', e=>self.skipWaiting());
-self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
