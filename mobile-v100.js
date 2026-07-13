911b34b0a8ce5c890b0493bce3f8fa751700d5c3
princemuysser-jpg <prince.muysser@gmail.com>
2026-07-13 12:05:22 +0300

    Delete mobile-v100.js

--- mobile-v100.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
-  document.querySelectorAll('.alin100r-bottom button').forEach(btn=>{
-    btn.addEventListener('click',()=>{
-      document.querySelectorAll('.alin100r-bottom button').forEach(x=>x.classList.remove('active'));
-      btn.classList.add('active');
-    });
-  });
-});
