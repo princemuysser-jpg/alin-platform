186ac475f57e6618b9bd58da1a885704388786b3
princemuysser-jpg <prince.muysser@gmail.com>
2026-07-19 14:24:15 +0300

    1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='alin-v1.3.7-mobile-repair-2';
+const VERSION='alin-v1.3.8-merged-safe';
 const STATIC_CACHE=`${VERSION}-static`;
 const RUNTIME_CACHE=`${VERSION}-runtime`;
 const CORE=[
